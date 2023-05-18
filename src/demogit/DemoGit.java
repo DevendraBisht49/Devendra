0897d4cf6ee9eea116d974c06229e16df10e3249
Devendra <bishtdevendra49@gmail.com>
2023-05-18 11:40:54 -0400

    print statement added

--- src/demogit/DemoGit.java
+++ src/demogit/DemoGit.java
@@ -14,6 +14,7 @@ public class DemoGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("hello this is my first git operation");
         // TODO code application logic here
     }
     
